5f9773ad2d4008286fe742387f9350fb9911796f
rawdlite <rawdlite@gmail.com>
2024-07-01 08:37:07 +0100

    upgrade fzf

--- setup/bash/alias.bash
+++ setup/bash/alias.bash
@@ -2,3 +2,4 @@ alias cat="bat --plain"
 alias show=run-mailcap-rs 
 alias opp=run-mailcap-rs
 alias ope="run-mailcap-rs --action edit"
+alias myip="ip address show dev eth0 | grep 'inet ' | cut -d ' ' -f 6 | cut -d / -f 1"
--- setup/fzf/install.sh
+++ setup/fzf/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sudo apt install fzf -y
+#sudo apt install fzf -y #outdated
+git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/src/fzf
+$HOME/src/fzf/install --key-bindings --completion --no-update-rc
+sudo cp $HOME/Src/fzf/bin/fzf /usr/local/bin
 DIR=$PWD/$(dirname $0)
 if grep -Fq "fzf.bash" ~/.bashrc
 then
