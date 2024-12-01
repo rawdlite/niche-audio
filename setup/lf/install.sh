72db645a62bdc8694f6b1ecb2988a2382578afa1
rawdlite <rawdlite@gmail.com>
2024-12-01 06:25:14 +0100

    fixes

--- setup/lf/install.sh
+++ setup/lf/install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # reduce dependencies an make option for basic install
 sudo apt install mpv fd-find golang flac -y
+# update go 
+# dietpi-software reinstall 188
 export GOPATH=/usr/local
 env CGO_ENABLED=0 go install -ldflags="-s -w" github.com/gokcehan/lf@latest
 mkdir /etc/lf
 ln -s /root/src/niche-audio/setup/lf/lfrc /etc/lf/lfrc
-mkdir ~/.cargo/tmp
+mkdir -p ~/.cargo/tmp
 export CARGO_TARGET_DIR=~/.cargo/tmp
 export CARGO_INSTALL_ROOT=/usr/local
 #get path to script
@@ -13,7 +15,7 @@ DIR=$PWD/$(dirname $0)
 #sudo apt install mpv flac -y
 # install run-mailcap-rs
 #mkdir ~/src
-#git clone https://github.com/cglindkamp/run-mailcap-rs.git ~/src/run-mailcap-rs
+git clone https://github.com/cglindkamp/run-mailcap-rs.git ~/src/run-mailcap-rs
 #cd ~/src/run-mailcap-rs
 #cargo install
 #make
